f7a8836f9dd9891c63aa0f2901185098604de312
Allen Hughes <allen@brushycreekchurch.com>
2017-08-29 17:29:59 -0400

    initial commit

---
+++ remove-channels.ps1
@@ -0,0 +1,9 @@
+﻿Get-ChildItem | ForEach-Object {
+    if ($_.Extension -eq ".mov") {
+        $filename = $_.Name
+        $outputname = $_.Name + "-output.mov"
+        ffmpeg -i $filename -c:v copy -map_channel 0.2.0 -map_channel -0.2.1 $outputname
+        Remove-Item $filename
+        Rename-Item -Path $outputname -NewName $filename
+    }
+}
